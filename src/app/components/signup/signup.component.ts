9c040debc225ad9f008ce75952bf44129f50ee65
rejrescale <rajman.bind@rescaletechnologies.com>
2024-09-07 18:42:49 +0530

    added media query,change alert

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,61 +1,59 @@
-
 import { Component } from '@angular/core';
-import { FormControl, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  Validators,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 
-
-
-
-
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ReactiveFormsModule,HttpClientModule],
+  imports: [ReactiveFormsModule, HttpClientModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
+  constructor(private router: Router, private authService: AuthService) {}
+  email = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor(private router:Router,private authService:AuthService ){} 
-  email =new FormControl("",[
+  password = new FormControl('', [
     Validators.required,
-    Validators.email
-  ])
-
-  password = new FormControl("",[
-    Validators.required,
-    Validators.minLength(6)
-  ])
+    Validators.minLength(6),
+  ]);
 
   signupForm = new FormGroup({
-    email:this.email,
-    password:this.password
-  })
+    email: this.email,
+    password: this.password,
+  });
 
   signup() {
     const email = this.signupForm.value.email;
     const password = this.signupForm.value.password;
-  
+
     if (email && password) {
-      this.authService.signupUser(email, password)
-        .subscribe(
-          (response: any) => {
-            // console.log(response);
+      this.authService.signupUser(email, password).subscribe(
+        (response: any) => {
+          // console.log(response);
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.error('Signup failed', error);
+          alert(JSON.parse(error?.error).msg);
+          
+          if (JSON.parse(error?.error).msg === 'User already exists')
             this.router.navigate(['/login']);
-          },
-          (error) => {
-            console.error('Signup failed', error);
-            alert('Signup failed. Please try again.');
-          }
-        );
+        }
+      );
     } else {
       console.error('Email or password is missing');
       alert('Please fill in both email and password.');
     }
   }
-  reset(){
-    this.signupForm.reset()
+  reset() {
+    this.signupForm.reset();
   }
 }
